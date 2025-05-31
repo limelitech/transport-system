822fcc906fe3c7195c687b90d5e2bfcf0a6fa685
Kevin Kathei Mutunga <140655785+limelitech@users.noreply.github.com>
2025-05-31 09:51:56 +0300

    Update Trip.js

--- backend/models/Trip.js
+++ backend/models/Trip.js
@@ -1,1 +1,17 @@
-// Trip.js
+const mongoose = require('mongoose');
+
+const tripSchema = new mongoose.Schema({
+  materialType: String,
+  date: String,
+  truckNo: String,
+  deliveryNo: String,
+  tonnage: Number,
+  locationFrom: String,
+  locationTo: String,
+  fuelStation: String,
+  fuelLiters: Number,
+  fuelAmount: Number,
+  cess: Number,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Trip', tripSchema);
