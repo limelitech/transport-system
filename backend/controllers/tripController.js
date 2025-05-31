dbdb97a19eaab9295de6a534a2b5dbd73c0662fb
Kevin Kathei Mutunga <140655785+limelitech@users.noreply.github.com>
2025-05-31 09:53:11 +0300

    Update tripController.js

--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -1,1 +1,20 @@
-// tripController.js
+const Trip = require('../models/Trip');
+
+exports.createTrip = async (req, res) => {
+  try {
+    const trip = new Trip(req.body);
+    await trip.save();
+    res.status(201).json(trip);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getTrips = async (req, res) => {
+  try {
+    const trips = await Trip.find();
+    res.json(trips);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
