5bf0f78a68f31f1e2a6443b69e72468e7a85db78
Kevin Kathei Mutunga <140655785+limelitech@users.noreply.github.com>
2025-05-31 09:54:15 +0300

    Update tripRoutes.js

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,1 +1,8 @@
-// tripRoutes.js
+const express = require('express');
+const router = express.Router();
+const { createTrip, getTrips } = require('../controllers/tripController');
+
+router.post('/', createTrip);
+router.get('/', getTrips);
+
+module.exports = router;
